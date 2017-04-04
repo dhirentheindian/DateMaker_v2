de39f9b2b6a299fcce062a025b94b37604cb3106
HazyNick <ang.nik10@gmail.com>
2017-04-04 21:42:19 +0800

    Updated Restaurant class to Parcelabel
    Changed to parcelabel for ResultsActivity

--- app/src/main/java/com/example/jonghyun/datemaker_v2/Restaurant.java
+++ app/src/main/java/com/example/jonghyun/datemaker_v2/Restaurant.java
@@ -1,11 +1,19 @@
 package com.example.jonghyun.datemaker_v2;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by dev on 21/03/2017.
  */
 
-public class Restaurant {
-    private String title, openTime, closeTime, city, address, cuisine;
+public class Restaurant implements Parcelable {
+    private String title;
+    private String openTime;
+    private String closeTime;
+    private String city;
+    private String address;
+    private String cuisine;
     private int budget;
     public Restaurant(String title, String openTime, String closeTime, String city, String address, int budget, String cuisine) {
         this.title = title;
@@ -75,4 +83,56 @@ public class Restaurant {
     public void setCuisine(String cuisine) {
         this.cuisine = cuisine;
     }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "title='" + title + '\'' +
+                ", openTime='" + openTime + '\'' +
+                ", closeTime='" + closeTime + '\'' +
+                ", city='" + city + '\'' +
+                ", address='" + address + '\'' +
+                ", cuisine='" + cuisine + '\'' +
+                ", budget=" + budget +
+                '}';
+    }
+
+    protected Restaurant(Parcel in) {
+        title = in.readString();
+        openTime = in.readString();
+        closeTime = in.readString();
+        city = in.readString();
+        address = in.readString();
+        cuisine = in.readString();
+        budget = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(openTime);
+        dest.writeString(closeTime);
+        dest.writeString(city);
+        dest.writeString(address);
+        dest.writeString(cuisine);
+        dest.writeInt(budget);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Restaurant> CREATOR = new Parcelable.Creator<Restaurant>() {
+        @Override
+        public Restaurant createFromParcel(Parcel in) {
+            return new Restaurant(in);
+        }
+
+        @Override
+        public Restaurant[] newArray(int size) {
+            return new Restaurant[size];
+        }
+    };
 }
